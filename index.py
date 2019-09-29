1d57d91983cffd84b74f65c3c040af1d589562c3
Nikita <fedorovni1996@yandex.ru>
2019-09-29 15:16:13 +0500

    Добавил блок Конструктивные элементы

--- index.py
+++ index.py
@@ -6,33 +6,44 @@ def get_html(url):
     r = requests.get(url)
     return r.text
 
+def filter_number(num):
+    number = num.split(' ')
+    return ''.join(number)
+
 def get_column_name():
     with open('cmc.csv', 'a', newline='') as f:
         writer = csv.writer(f)
         col1 = 'Адрес'
-        col2 = "Год ввода в эксплуатацию"
-        col3 = "Детская площадка"
-        col4 = "Дом признан аварийным"
-        col5 = "Дополнительная информация"
-        col6 = "Количество жилых помещений"
-        col7 = "Класс энергетической эффективности"
-        col8 = "Количество лифтов"
-        col9 = "Количество подъездов"
-        col10 = "Лифты"
-        col11 = "Наибольшее количество этажей"
-        col12 = "Наименьшее количество этажей"
-        col13 = "Количество нежилых помещений"
-        col14 = "Площадь жилых помещений м2"
-        col15 = "Площадь нежилых помещений м2"
-        col16 = "Площадь помещений общего имущества м2"
-        col17 = "Площадь зем. участка общего имущества м2"
-        col18 = "Площадь парковки м2"
-        col19 = "Серия, тип постройки здания"
-        col20 = "Спортивная площадка"
-        col21 = "Формирование фонда кап. ремонта"
-        col22 = "Тип дома"
-
-
+        col2 = 'Год ввода в эксплуатацию'
+        col3 = 'Детская площадка'
+        col4 = 'Дом признан аварийным'
+        col5 = 'Дополнительная информация'
+        col6 = 'Количество жилых помещений'
+        col7 = 'Класс энергетической эффективности'
+        col8 = 'Количество лифтов'
+        col9 = 'Количество подъездов'
+        col10 = 'Лифты'
+        col11 = 'Наибольшее количество этажей'
+        col12 = 'Наименьшее количество этажей'
+        col13 = 'Количество нежилых помещений'
+        col14 = 'Площадь жилых помещений м2'
+        col15 = 'Площадь нежилых помещений м2'
+        col16 = 'Площадь помещений общего имущества м2'
+        col17 = 'Площадь зем. участка общего имущества м2'
+        col18 = 'Площадь парковки м2'
+        col19 = 'Серия, тип постройки здания'
+        col20 = 'Спортивная площадка'
+        col21 = 'Формирование фонда кап. ремонта'
+        col22 = 'Тип дома'
+        col23 = 'Оборудование'
+        col24 = 'Количество мусоропроводов, ед.'
+        col25 = 'Несущие стены'
+        col26 = 'Площадь подвала, кв.м'
+        col27 = 'Крыша'
+        col28 = 'Мусоропровод'
+        col29 = 'Перекрытия'
+        col30 = 'Фасад'
+        col31 = 'Фундамент'
 
         writer.writerow((col1,
                         col2,
@@ -55,10 +66,19 @@ def get_column_name():
                         col19,
                         col20,
                         col21,
-                        col22))
+                        col22,
+                        col23,
+                        col24,
+                        col25,
+                        col26,
+                        col27,
+                        col28,
+                        col29,
+                        col30,
+                        col31))
                         
 def write_csv(data):
-    with open('cmc.csv', 'a', newline='') as f:
+    with open('cmc.csv', 'a', encoding='utf-8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow((data['address'],
                         data['year'],
@@ -81,7 +101,16 @@ def write_csv(data):
                         data['series_type_construction'],
                         data['sports_ground'],
                         data['formation_capital_repair'],
-                        data['type_building']))
+                        data['type_building'],
+                        data['equipment'],
+                        data['number_chutes'],
+                        data['bearing_wall'],
+                        data['square_basement'],
+                        data['roof'],
+                        data['refuse_chute'],
+                        data['overlapping'],
+                        data['facade'],
+                        data['foundation']))
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -109,6 +138,14 @@ def get_page_data(html):
     sports_ground = '-'
     formation_capital_repair = '-'
     type_building = '-'
+    equipment = '-'
+    number_chutes = '-'
+    square_basement = '-'
+    roof = '-'
+    refuse_chute = '-'
+    overlapping = '-'
+    facade = '-'
+    foundation = '-'
 
     trs = soup.find_all('tr')
     for tr in trs:
@@ -120,13 +157,13 @@ def get_page_data(html):
 
             if (tds[i].text == 'Детская площадка'):
                 child_yard = tds[-1].text
-            
+
             if (tds[i].text == 'Дом признан аварийным'):
                 emergency_house = tds[-1].text
-            
+
             if (tds[i].text == 'Дополнительная информация'):
                 more_info = tds[-1].text
-            
+
             if (tds[i].text == 'Количество жилых помещений'):
                 number_apartments = tds[-1].text
 
@@ -144,39 +181,66 @@ def get_page_data(html):
 
             if (tds[i].text == 'Наибольшее количество этажей'):
                 max_floor = tds[-1].text
-            
+
             if (tds[i].text == 'Наименьшее количество этажей'):
                 min_floor = tds[-1].text
-            
+
             if (tds[i].text == 'Количество нежилых помещений'):
                 number_apartments_without_people = tds[-1].text
 
             if (tds[i].text == 'Площадь жилых помещений м2'):
-                square_number_apartments = tds[-1].text
+                square_number_apartments = filter_number(tds[-1].text)
 
             if (tds[i].text == 'Площадь нежилых помещений м2'):
-                square_number_apartments_without_people = tds[-1].text
-            
+                square_number_apartments_without_people = filter_number(tds[-1].text)
+
             if (tds[i].text == 'Площадь помещений общего имущества м2'):
-                square_common_property = tds[-1].text
+                square_common_property = filter_number(tds[-1].text)
 
             if (tds[i].text == 'Площадь зем. участка общего имущества м2'):
-                land_square_common_property = tds[-1].text
-            
+                land_square_common_property = filter_number(tds[-1].text)
+
             if (tds[i].text == 'Площадь парковки м2'):
-                square_parking = tds[-1].text
-            
+                square_parking = filter_number(tds[-1].text)
+
             if (tds[i].text == 'Серия, тип постройки здания'):
                 series_type_construction = tds[-1].text
-            
+
             if (tds[i].text == 'Спортивная площадка'):
                 sports_ground = tds[-1].text
-            
+
             if (tds[i].text == 'Формирование фонда кап. ремонта'):
                 formation_capital_repair = tds[-1].text
-            
+
             if (tds[i].text == 'Тип дома'):
                 type_building = tds[-1].text
+
+            if (tds[i].text == 'Оборудование'):
+                equipment = tds[-1].text
+
+            if (tds[i].text == 'Количество мусоропроводов, ед.'):
+                number_chutes = tds[-1].text
+
+            if (tds[i].text == 'Несущие стены'):
+                bearing_wall = tds[-1].text
+
+            if (tds[i].text == 'Площадь подвала, кв.м'):
+                square_basement = filter_number(tds[-1].text)
+
+            if (tds[i].text == 'Крыша'):
+                roof = tds[-1].text
+
+            if (tds[i].text == 'Мусоропровод'):
+                refuse_chute = tds[-1].text
+
+            if (tds[i].text == 'Перекрытия'):
+                overlapping = tds[-1].text
+
+            if (tds[i].text == 'Фасад'):
+                facade = tds[-1].text
+
+            if (tds[i].text == 'Фундамент'):
+                foundation = tds[-1].text
             i = i + 1
 
     data = {'address': address, 
@@ -200,12 +264,20 @@ def get_page_data(html):
             'series_type_construction': series_type_construction,
             'sports_ground': sports_ground,
             'formation_capital_repair': formation_capital_repair,
-            'type_building': type_building}
+            'type_building': type_building,
+            'equipment': equipment,
+            'number_chutes': number_chutes,
+            'bearing_wall': bearing_wall,
+            'square_basement': square_basement,
+            'roof': roof,
+            'refuse_chute': refuse_chute,
+            'overlapping': overlapping,
+            'facade': facade,
+            'foundation': foundation}
     write_csv(data) 
         
 def main():
-    # url = 'http://dom.mingkh.ru/chelyabinskaya-oblast/chelyabinsk/538552'
-    url = 'http://dom.mingkh.ru/chelyabinskaya-oblast/chelyabinsk/535117'
+    url = 'http://dom.mingkh.ru/chelyabinskaya-oblast/chelyabinsk/538552'
     get_column_name()
     get_page_data(get_html(url))
 
